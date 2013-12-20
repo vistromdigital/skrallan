774080f86a8e2a279d0676c56fc23cca8211549d
Jesper Stråhle <jesper@vistromwebb.se>
2013-12-20 18:04:54 +0100

    Add first version.

---
+++ skrallan.js
@@ -0,0 +1,64 @@
+(function () {
+	var currentDirection,
+		currentOffset,
+		previousDirection,
+		previousOffset = window.pageYOffset,
+		scrollDownEvent,
+		scrollUpEvent,
+		scrollDownStartEvent,
+		scrollUpStartEvent;
+
+	// Initialize events
+	try {
+		scrollDownEvent = new Event('scrolldown');
+		scrollUpEvent = new Event('scrollup');
+		scrollDownStartEvent = new Event('scrolldownstart');
+		scrollUpStartEvent = new Event('scrollupstart');
+	} catch (e) {
+		// No browser support for Event constructor, use
+		// the old-fashioned way :(
+
+		scrollDownEvent = document.createEvent('Event');
+		scrollUpEvent = document.createEvent('Event');
+		scrollDownStartEvent = document.createEvent('Event');
+		scrollUpStartEvent = document.createEvent('Event');
+
+		scrollDownEvent.initEvent('scrolldown', true, true);
+		scrollUpEvent.initEvent('scrollup', true, true);
+		scrollDownStartEvent.initEvent('scrolldownstart', true, true);
+		scrollUpStartEvent.initEvent('scrollupstart', true, true);
+	}
+
+	function onScroll(event) {
+		currentOffset = window.pageYOffset;
+
+		// Determine the current scroll direction
+		if (currentOffset < previousOffset) {
+			currentDirection = 'up';
+		} else if (currentOffset > previousOffset) {
+			currentDirection = 'down';
+		} else {
+			currentDirection = undefined;
+		}
+
+		// Has the scroll direction changed since the last scroll?
+		if (currentDirection !== previousDirection) {
+			if (currentDirection === 'up') {
+				window.dispatchEvent(scrollUpStartEvent);
+			} else if (currentDirection === 'down') {
+				window.dispatchEvent(scrollDownStartEvent);
+			}
+		}
+
+		if (currentDirection === 'up') {
+			window.dispatchEvent(scrollUpEvent);
+		} else if (currentDirection === 'down') {
+			window.dispatchEvent(scrollDownEvent);
+		}
+
+		previousDirection = currentDirection;
+		previousOffset = currentOffset;
+	}
+
+	window.addEventListener('scroll', onScroll, false);
+})();
